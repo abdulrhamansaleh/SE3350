0fa73935b755b69e0d5eee432fb5e22590e2c3d1
AugustZilakovs <106542089+Term-AZ@users.noreply.github.com>
2024-01-26 11:06:03 -0500

    feat!: Connected db

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cors = require('cors')
+const cookieParser = require('cookie-parser');
+var app = express();
+const db = require('../db/db.js')
+const dotenv = require('dotenv')
+const bcrypt = require('bcrypt')
+const saltRounds=10
+var generate_token = require('./jwt/generate_token.js')
+var generate_refresh = require('./jwt/generate_refresh.js')
+var validate_token = require('./jwt/validate_token.js')
+const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
+var validate_email = require('./validate/validate_email.js')
+var validate_password = require('./validate/validate_password.js')
