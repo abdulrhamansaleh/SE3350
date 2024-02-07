a1ff583cbbe2399484a8cd50bab7e62c0bbe69f2
AugustZilakovs <106542089+Term-AZ@users.noreply.github.com>
2024-02-07 15:07:30 -0500

    fix!: fixed table script

--- db/schema.session.sql
+++ db/schema.session.sql
@@ -1,7 +1,7 @@
 -- Accounts table
 CREATE TABLE Accounts (
     account_id INT AUTO_INCREMENT PRIMARY KEY,
-    type ENUM ('parent', 'child', 'employee') NOT NULL,
+    type ENUM ('parent', 'child', 'employee', 'admin') DEFAULT 'parent' NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -26,7 +26,7 @@ CREATE TABLE EmployeeDetails (
 CREATE TABLE ParentDetails (
     parent_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     account_id INT NOT NULL UNIQUE,
-    parent_number VARCHAR(50),
+    parent_number VARCHAR(11) NOT NULL,
     FOREIGN KEY (account_id) REFERENCES Accounts(account_id) ON DELETE RESTRICT
 );
 
