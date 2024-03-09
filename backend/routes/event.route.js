e44bdd8c36430d663ea74e794ce7720e19b6ecdc
abdulrhamansaleh <asaleh64@uwo.ca>
2024-03-08 19:00:13 -0500

    initial skeleton

---
+++ backend/routes/event.route.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/db');
+
+/* Events for display */
+router.get('running-events', async(request, response) => {
+
+})
+
+/* Add Events to Calendar */
+router.post('create-event', async(request, response) => {
+
+})
+/* Remove Events from Calendar */
+router.delete('remove-event', async(request, response) => {
+
+})
+/* Edit Events In Calendar */
+router.patch('edit-event', async(request,response) => {
+
+})
